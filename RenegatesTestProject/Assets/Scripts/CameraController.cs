009a4f46293ebc8c37f1a4cc6465e2ad8a51c660
ArkashaPygachev <93211239@mail.ru>
2021-07-26 18:41:54 +0300

    Camera controller

---
+++ RenegatesTestProject/Assets/Scripts/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Vector3 camOffset;
+    public Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position + camOffset;
+    }
+}
