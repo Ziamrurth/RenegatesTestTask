fe0500560ccc28ee0df621a87d152216b9f45e0f
ArkashaPygachev <93211239@mail.ru>
2021-07-26 19:56:41 +0300

    Animationes added

--- RenegatesTestProject/Assets/Scripts/PlayerController.cs
+++ RenegatesTestProject/Assets/Scripts/PlayerController.cs
@@ -8,16 +8,25 @@ public class PlayerController : MonoBehaviour
     private Camera cam;
 
     private NavMeshAgent agent;
+    private Animator anim;
+
+    private bool isMoving = false;
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
         cam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (agent.desiredVelocity == Vector3.zero && isMoving)
+        {
+            isMoving = false;
+        }
+
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
@@ -26,7 +35,11 @@ public class PlayerController : MonoBehaviour
             if(Physics.Raycast(ray, out hit))
             {
                 agent.SetDestination(hit.point);
+
+                isMoving = true;
             }
         }
+
+        anim.SetBool("IsMoving", isMoving);
     }
 }
